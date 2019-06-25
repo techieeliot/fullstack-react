9e3ec1f475a069db874bb486e4aa92ef9ff03e05
Eliot Sanford <eliot587@gmail.com>
2019-06-25 16:33:51 -0500

    attempt one at creating an arrow furction for the princess output

--- es6-beau/className.js
+++ es6-beau/className.js
@@ -1,10 +1,21 @@
 class Princess {
-    constructor(name, movie)
-    this.name = name;
-    this.movie = movie
+    constructor(name, movie){
+        this.name = name;
+        this.movie = movie
+    }
 }
+   
 
 let jasmine = new Princess('Jasmin', "Aladin");
 
 console.log(jasmine);
+
+const articleCenter = document.getElementsByClassName('center');
+
+articleCenter.addEventListener('click', outputPrincess());
+
+const outputPrincess () => x = articleCenter.innnHTML('<section><h1>${name}</h1></section>');
+
+
+
 
