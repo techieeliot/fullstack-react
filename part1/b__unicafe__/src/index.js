42111ba33ed99a64716a445e1bd0687c12ea3197
Eliot Sanford <eliot587@gmail.com>
2019-07-23 23:27:03 -0500

    map function for VoteTally Component

--- part1/b__unicafe__/src/index.js
+++ part1/b__unicafe__/src/index.js
@@ -1,26 +1,28 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-        const stats = {
-            title: `give feedback`,
-            vote: [
-                {
-                    type: `good`,
-                    count: 0
-                },
-                {
-                    type: `neutral`,
-                    count: 0
-                },
-                {
-                    type: `bad`,
-                    count: 0
-                },
-                
-            ]
-        }
-        
-        console.log(stats.vote);
 
+const stats = {
+    title: `give feedback`,
+    votes: [
+        {
+            type: `good`,
+            count: 0
+        },
+        {
+            type: `neutral`,
+            count: 0
+        },
+        {
+            type: `bad`,
+            count: 0
+        },
+        
+    ]
+}
+
+        
+console.log(stats.votes);
+        
 const Heading = props => <div><h1>{props.stats.title}</h1></div>
 
 const Button = (props) => (
@@ -30,60 +32,65 @@ const Button = (props) => (
 
 )
 
-const VoteTally = () => (
-    <div>Vote Tally</div>  
+const TotalVotes = (props) => props.stats.votes.reduce((total, vote) => {console.log(vote); return (total + vote)}, 0)
+console.log(TotalVotes);
+
+
+const VoteTally = (props) => {
+    return (
+        <div>
+            <h1>statistics</h1>
+            {props.stats.votes.map((votes) => <p key={votes.type}>{votes.type} {votes.count}</p>)}
+            {/* {props.stats.votes.map((votes) => <p key={votes.type}>{votes.type} {votes.count}</p>)} */}
+        </div> 
     )
-    
+
+}
+const Average = (props) => (
+    <div>
+        <p>average {(props.stats.votes[0].count - props.stats.votes[2].count) / props.stats.votes.length}</p>
+    </div>
+)    
+// console.log(stats.votes.reduce((a=, b) => a + b, 0));
+
+
+const Positive = (props) => (
+    <div>
+        <p key={props.stats.votes.count}>positive {(props.stats.votes.count / TotalVotes)*100}%</p>
+    </div>
+)
     
     const App = () => {
         
         
         const [count, setCount] = useState(0)
         
-        const Feedback = (props) => {
-                    console.log(props);
-                    return (
-                        <div className="give-feedback"> 
-                        
-                            <Heading stats={stats} /> 
-                            <Button stats={stats} handleClick={() => setCount(stats.vote[0].count += 1)} text={stats.vote[0].type} /> 
-                            <Button stats={stats} text={stats.vote[1].type}/>
-                            <Button stats={stats} text={stats.vote[2].type} /> 
-                        </div>
-                    ) 
-                        
-                    }
-                const Stats = (props) => {
-                    console.log(props);
-                    return (
-                    <div className="stats"> 
-                        <h1>statistics</h1>
-                        <p>{stats.vote[0].type} {props.stats.vote[0].count}</p>
-                        <p>{stats.vote[1].type} {props.stats.vote[1].count}</p>
-                        <p>{stats.vote[2].type} {props.stats.vote[2].count}</p>
-                        <p>average 0</p>
-                        <p>positive 0%</p> 
-                    </div>
-                    ) 
-                    
-                }
-        // save clicks of each button to own state
 
-    
-        function useIncrement() {
-            const [count, setCount] = useState(0);
-            const increment = () => setCount(count + 1);
-            return (count, increment )
-            
-        } 
-        
-        const Counter = () => {
-            const {increment} = useIncrement()
-            return <button onClick={increment}>good</button>
-        
+        const Feedback = (props) => {
+            console.log(props);
+            return (
+                <div className="give-feedback"> 
+                
+                    <Heading stats={stats} /> 
+                    <Button stats={stats} handleClick={() => setCount(stats.votes[0].count += 1)} text={stats.votes[0].type} /> 
+                    <Button stats={stats} handleClick={() => setCount(stats.votes[1].count += 1)} text={stats.votes[1].type} /> 
+                    <Button stats={stats} handleClick={() => setCount(stats.votes[2].count += 1)} text={stats.votes[2].type} /> 
+                </div>
+            ) 
+                    
         }
 
-        
+        const Stats = (props) => {
+            console.log(props);
+            return (
+            <div className="stats"> 
+                <VoteTally stats={stats} />
+                <Average stats={stats} />
+                <Positive stats={stats} />
+            </div>
+            ) 
+            
+        }
 
   return (
     <div>
