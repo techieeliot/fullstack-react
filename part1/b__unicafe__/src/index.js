3a210d6adf7abf98aa93a84eb6f183940778b046
Eliot Sanford <eliot587@gmail.com>
2019-07-26 08:05:28 -0500

    included recordings of yesterday's standup for others

--- part1/b__unicafe__/src/index.js
+++ part1/b__unicafe__/src/index.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-    
+const Stats = (props) => {
+    console.log(props);
+    return(        
+        <div>
+            <p>good {props.good}</p>
+            <p>neutral {props.neutral}</p>
+            <p>bad {props.bad}</p>
+            <p>all {props.total}</p> 
+            <p>average {props.average}</p>
+            <p>positive {props.positive}%</p>
+        </div> 
+    )
+}
+
 const App = () => {
-
-
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    
+
     const total = good + neutral + bad;
     const average = (good - bad) / total;
     const positive = (good/total)*100;
@@ -17,24 +28,31 @@ const App = () => {
     const handleNeutral = (value) => setNeutral(value)
     const handleBad = (value) => setBad(value)
 
+    const display = (total) ? <Stats 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+    />
+    : "No Feedback Given";
+
 
   return (
     <div>
-        <h1>give feeback</h1>
-        <button onClick={()=> handleGood(good + 1)}>good</button>
-        <button onClick={()=> handleNeutral(neutral + 1)}>neutral</button>
-        <button onClick={()=> handleBad(bad + 1)}>bad</button>
+        <div> 
+            <h1>give feeback</h1>
+            <button onClick={()=> handleGood(good + 1)}>good</button>
+            <button onClick={()=> handleNeutral(neutral + 1)}>neutral</button>
+            <button onClick={()=> handleBad(bad + 1)}>bad</button>
+        </div>
 
 
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {total}</p> 
-        <p>average {average}</p>
-        <p>positive {positive}%</p>
-
-        
+        <div>
+            <h1>statistics</h1>
+            {display}
+        </div>        
     </div>
   )
 }
